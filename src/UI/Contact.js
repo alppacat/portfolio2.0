7e298a4e66e3a50147df8b1b941b7f416111b84e
alppacat <lorena.pratts@gmail.com>
2019-01-21 13:50:46 -0600

    Componente consola listo

--- src/UI/Contact.js
+++ src/UI/Contact.js
@@ -7,31 +7,34 @@ const Contact = () => {
     const body = () => {
         return(
         <div className="console-body-contact">
-            <section className="section">
+            <section className="section p-margin">
                 <span>Let's turn ideas into reality!</span>
                 <p>¿Te gustaría trabajar juntos?
                     ¿o simplemente quisieras saber en qué proyecto me encuentro trabajando?,
                     ¿Tal vez solo quieras saludar?</p>
                     <p>¡No dudes en escribirme un mensaje!</p>
+
+                    <section className="section lista">
                 <ul>
-                    <li>
+                    <li className="list-items">
                         <a href="mailto:tzupratts@gmail.com"> 
                         <i class="fas fa-envelope"/>{" "}
                         tzupratts@gmail.com</a>
                     </li>
-                    <li>
+                    <li className="list-items">
                     <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/tzupratts/">
                     <i class="fab fa-linkedin" />{" "}
                     @tzupratts
                     </a>
                     </li>
-                    <li>
+                    <li className="list-items">
                     <a target="_blank" rel="noopener noreferrer" href="https://github.com/alppacat">
                     <i class="fab fa-github" />{" "}
                      @alppacat
                     </a>
                     </li>
                 </ul>
+                </section>
             </section>
         </div>
         )
