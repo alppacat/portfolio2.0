bdff82096fb24229337b05b6681a5f70d5cad55d
alppacat <lorena.pratts@gmail.com>
2019-01-23 18:55:46 -0600

    display de proyectos listo

--- src/UI/Projects.js
+++ src/UI/Projects.js
@@ -19,23 +19,90 @@ class Projects extends Component {
 
     }
 
+    renderProject = () => {
+        const mainProject = this.state.projects[1];
+
+        let project;
+
+       if(mainProject === lux){
+           const currentProject = {
+               name: "Lux",
+               description: "Lux es una red social creada en equipo, cuyo objetivo  princial es la creación de espacios seguros así como redes de apoyo emocional para personas que padeceden y/o sufren de depresión y/o ansiedad.",
+               tecnologias: "JavaScript Vanilla, Firebase, Bootstrap, HTML5, CSS3.",
+               demo: 'https://alppacat.github.io/cdmx-2018-06-bc-core-am-social-network/src',
+               repositorio: 'https://github.com/alppacat/cdmx-2018-06-bc-core-am-social-network'
+           }
+
+            project = currentProject
+    
+       }else if(mainProject === rickAndMorty){
+           const currentProject = {
+               name: "Rick and Morty App",
+               description: "Aplicación nativa que permite visualizar a los personajes de la serie, así como información adicional de estos. También permite ver un listado de los episodios de la primera temporada.",
+               tecnologias: "React Native, Native Base, React Navigation, Rick and morty API, Android Studio, Expo.",
+               demo: 'https://expo.io/@tzupratts/rick-and-morty-react-native',
+               repositorio: 'https://github.com/alppacat/rick-and-morty'
+           }
+
+           project = currentProject
+
+       } else if(mainProject === ghibli){
+           const currentProject = {
+               name: "Ghibli Movies",
+               description: "Aplicación web que permite visualizar las películas realizadas por el estudio Ghibli, así como una pequeña descripción de estas.",
+               tecnologias: "JavaScript Vanilla, Ghibli API, HTML5, CSS3.",
+               demo: 'https://alppacat.github.io/Studio-Ghibli-/',
+               repositorio: 'https://github.com/alppacat/Studio-Ghibli-'
+           }
+            project = currentProject
+
+       }
+
+        return(
+            <section className="section-projects">
+                <div className="container-projects-info">
+                    <h3>{project.name}</h3>
+                    <div>
+                        <p>{project.description}</p>
+                        <span>Tecnologías implementadas:</span>
+                        <p>{project.tecnologias}</p>
+                            <div className="external-links"> 
+                                <a target="_blank" rel="noopener noreferrer" href={project.demo}>
+                                <i className="fas fa-external-link-alt"></i> {" "}
+                                demo
+                                </a>
+                                <a target="_blank" rel="noopener noreferrer" href={project.repositorio}>
+                                <i className="fab fa-github"></i> {" "}
+                                código
+                                </a>
+                            </div>
+                        
+                    </div>
+                </div>
+            </section>
+        )
+
+    }
+
     render() { 
         return ( <section id="projects">
-            <div className="section container-images">
+            <div className="section container-projects">
 
-                <div>
-                    <img className="images-project" src={this.state.projects[0]}/>
+                <div className="container-images side-project-left">
+                    <img className="images-project side-image" src={this.state.projects[0]}/>
                 </div>
-                <div>
+                <div className="container-images main-project">
                     <img className="images-project" src={this.state.projects[1]}/>
                 </div>
-                <div>
-                    <img className="images-project" src={this.state.projects[2]}/>
+                <div className="container-images side-project-right">
+                    <img className="images-project side-image" src={this.state.projects[2]}/>
                     </div>
-                    <button onClick={this.slideProjects}>Next</button>
-
-
             </div>
+            {this.renderProject()}
+
+                <div className="next-container" onClick={this.slideProjects}>
+                 <p className="next-icon">>></p>
+                 </div>
         </section> );
     }
 }
