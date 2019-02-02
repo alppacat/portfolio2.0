3004484488154b838cf5728927128d09ddc4bd4d
alppacat <lorena.pratts@gmail.com>
2019-02-01 20:21:00 -0600

    Updates

---
+++ precache-manifest.a94ecd3c6b1f9f5bdae2b461f61abaa3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "83eed1b2dfa40b0fb863",
+    "url": "/static/css/main.0cc3294a.chunk.css"
+  },
+  {
+    "revision": "83eed1b2dfa40b0fb863",
+    "url": "/static/js/main.83eed1b2.chunk.js"
+  },
+  {
+    "revision": "30445464c348663456a6",
+    "url": "/static/js/1.30445464.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cec4a45f87f538f41d82c84335351a0b",
+    "url": "/static/media/Old-Display-lux.cec4a45f.gif"
+  },
+  {
+    "revision": "64f0f14508fd9925f1ff35fc6df4c718",
+    "url": "/static/media/Old-PDA-rick.64f0f145.gif"
+  },
+  {
+    "revision": "82c2a6c846f65983f39bf251b9ca60d9",
+    "url": "/static/media/Old-Display-ghibli.82c2a6c8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "979ab808c33450651c32f175d5c0d52b",
+    "url": "/static/media/30577992388_b5e447777e_o.979ab808.jpg"
+  },
+  {
+    "revision": "dd75a16307c32cd39c7d70f87aeee825",
+    "url": "/index.html"
+  }
+];
