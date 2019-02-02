af4d9460563a89ac9501bcb419fe83f9c52a3d6f
alppacat <lorena.pratts@gmail.com>
2019-02-01 20:10:30 -0600

    Final version

--- src/UI/Resume.js
+++ src/UI/Resume.js
@@ -11,7 +11,7 @@ const Resume = (props) => {
       </header>
       <section className="modal-card-body">
       <div className="external-links">
-        <a href="">Descarga</a>
+        <a href="https://github.com/alppacat/portfolio2.0/raw/branchQ/src/assets/cv_tzu_pratts.pdf">Descarga</a>
         <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1jqS2L_zjVi7-Np2sjY45RWdAgguq3OCa/view?usp=sharing">Ver Online</a>
         </div>
       </section>
